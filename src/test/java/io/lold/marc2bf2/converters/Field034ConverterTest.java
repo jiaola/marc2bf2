2121780e64d3600521a43083369679150f8d9538
Dazhi Jiao <djiao@iu.edu>
2017-04-08 07:19:04 -0400

    field 034

---
+++ src/test/java/io/lold/marc2bf2/converters/Field034ConverterTest.java
@@ -0,0 +1,85 @@
+package io.lold.marc2bf2.converters;
+
+import io.lold.marc2bf2.vocabulary.BIB_FRAME;
+import io.lold.marc2bf2.vocabulary.BIB_FRAME_LC;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.marc4j.marc.DataField;
+import org.marc4j.marc.Record;
+import org.marc4j.marc.Subfield;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
+public class Field034ConverterTest {
+    Field034Converter converter;
+    private Model model;
+
+    @Parameterized.Parameter
+    public Record record;
+
+    @Parameterized.Parameters
+    public static Record[] records() {
+        return TestUtils.readTestRecords("ConvSpec-010-048/marc.xml");
+    }
+
+    @Before
+    public void setUp() {
+        model = io.lold.marc2bf2.ModelFactory.createBfModel();
+        // create a mock work and adminmetadata
+        model.createResource(ModelUtils.getUri(record, "Work"))
+                .addProperty(RDF.type, BIB_FRAME.Work)
+                .addProperty(BIB_FRAME.adminMetadata, model.createResource()
+                        .addProperty(RDF.type, BIB_FRAME.AdminMetadata));
+        converter = new Field034Converter(model, record);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        model.close();
+        model = null;
+        converter = null;
+    }
+
+    @Test
+    public void testConvertSFA() throws Exception {
+        String q = String.join("\n"
+                , "PREFIX bf: <" + BIB_FRAME.getURI() + ">"
+                , "PREFIX rdf: <" + RDF.getURI() + ">"
+                , "PREFIX rdfs: <" + RDFS.getURI() + ">"
+                , "PREFIX bflc: <" + BIB_FRAME_LC.getURI() + ">"
+                , "SELECT ?w  "
+                , "WHERE { "
+                , "  ?x bf:note ?y ."
+                , "  ?y rdfs:label \"%1s\" ."
+                , "}");
+        List<DataField> fields = record.getDataFields();
+        for (DataField field: fields) {
+            if (field.getTag().equals("034")) {
+                model = converter.convert(field);
+                model.write(System.out);
+                List<Subfield> sfs = field.getSubfields('a');
+                if (field.getSubfields('b').isEmpty() && field.getSubfields('c').isEmpty()) {
+                    for (Subfield a: sfs) {
+                        if ("a".equals(a.getData())) {
+                            ResultSet rs = TestUtils.sparql(String.format(q, "linear scale"), model);
+                            assertTrue(rs.hasNext());
+                        }
+                    }
+                }
+            } else {
+                assertEquals(model, converter.convert(field)); // model shouldn't be changed
+            }
+        }
+    }
+}
